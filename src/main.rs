f705597d0b6c37cb4fbca0c93dd21c6f9a79f8e4
= <yumin-wang@gmail.com>
2020-01-06 11:06:07 +0800

    增添actix-web 2.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate fluffy;
+
+use actix_web::{App, HttpServer, middleware};
+use tera::Tera;
+
+mod config;
+mod controllers;
+
+use controllers::{index};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    //地址/端口
+    let host_port = &format!("{}:{}", config::BIND_HOST, config::BIND_PORT);
+
+    HttpServer::new(|| {
+
+        let tpl = tmpl!("/templates/**/*");
+        
+        App::new()
+            .data(tpl)
+            .wrap(middleware::Logger::default()) // enable logger
+            .service(index::index)
+            .service(index::manage)
+            .service(index::right)
+    })
+    .bind(host_port)?
+    .run()
+    .await
 }

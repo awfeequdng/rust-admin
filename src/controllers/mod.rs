eaa80daaca9001a9514531eb29689d3566657cf5
hanting <hanting.hou@gmail.com>
2020-01-11 13:27:17 +0800

    增加自动保存方法

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,16 +1,14 @@
 use std::collections::HashMap;
-use fluffy::{ tmpl::Tpl, response };
+use fluffy::{ tmpl::Tpl, response, model::Model, model::Db, data_set::DataSet, db, };
 use crate::models::ModelBackend;
 use actix_web::{HttpResponse, web};
+use crate::caches;
 
 pub trait Controller { 
 
     type M: ModelBackend;
 
-    fn get_controller_name() -> &'static str { 
-        let paths: Vec<&'static str> = std::any::type_name::<Self>().rsplit("::").collect();
-        paths[0]
-    }
+    fn get_controller_name() -> &'static str;
     
     /// 主頁
     fn index(tpl: Tpl) -> HttpResponse { 
@@ -22,14 +20,36 @@ pub trait Controller {
             "records" => &info.records,
             "pager" => &info.pager,
         ];
-        let view_file =  &format!("{}/index.html", controller_name);
+        let view_file = &format!("{}/index.html", controller_name);
+        render!(tpl, view_file, &data)
+    }
+
+    /// 編輯
+    fn edit(tpl: Tpl) -> HttpResponse { 
+        let controller_name = Self::get_controller_name(); //控制器名称
+        let data = tmpl_data![
+            "controller_name" => &controller_name,
+        ];
+        let view_file = &format!("{}/edit.html", controller_name);
         render!(tpl, view_file, &data)
     }
     
     /// 添加
     fn create(post: web::Form<HashMap<String, String>>) -> HttpResponse { 
-        println!("{:?}", post);
-        response::ok()
+        let table_name = Self::M::get_table_name();
+        let table_fields = caches::TABLE_FIELDS.lock().unwrap();
+        let post_fields = post.into_inner();
+        let checked_fields = Db::check_fields(table_name, &table_fields, post_fields, false); //經過檢驗之後的數據
+        let mut data = DataSet::create();
+        for (k, v) in &checked_fields { 
+            data.set(k, v);
+        }
+        let mut conn = db::get_conn();
+        let id = Self::M::create(&mut conn, &data);
+        if id > 0 { 
+            return response::ok();
+        } 
+        response::error("增加記錄失敗")
     }
     
     /// 修改

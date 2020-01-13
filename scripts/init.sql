3b98359730524b99b4c5d67d6b2f80ec39031119
hanting <hanting.hou@gmail.com>
2020-01-13 17:54:10 +0800

    增加多个新表

--- scripts/init.sql
+++ scripts/init.sql
@@ -49,3 +49,57 @@ CREATE TABLE IF NOT EXISTS admin_roles (
 );
 INSERT INTO admin_roles (name, remark, menu_ids) VALUES 
 ('系统管理员', '后台用户管理', '[{"id": 1, "menus": [{"id": 2}]}]');
+
+/** 前台用户 **/
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL DEFAULT '' COMMENT '用户名称',
+    password CHAR(32) NOT NULL DEFAULT '' COMMENT '登錄密碼',
+    secret CHAR(32) NOT NULL DEFAULT '' COMMENT '用户密钥',
+    mail VARCHAR(100) NOT NULL DEFAULT '' COMMENT '电子邮件',
+    last_ip VARCHAR(32) NOT NULL DEFAULT '' COMMENT '最後IP',
+    state TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '狀態',
+    login_count INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '登錄次數',
+    last_login INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '上次登錄時間',
+    created INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '創建時間',
+    updated INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '修改時間',
+    PRIMARY KEY(id),
+    INDEX(name)
+);
+INSERT INTO users (name, password, mail, created, updated) VALUES 
+('user', md5('qwe123'), 'abc@gmail.com', UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
+
+/** 视频分类 **/
+DROP TABLE IF EXISTS video_categories;
+CREATE TABLE IF NOT EXISTS video_categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL DEFAULT '' COMMENT '名称',
+    remark VARCHAR(100) NOT NULL DEFAULT '' COMMENT '备注',
+    PRIMARY KEY(id)
+);
+INSERT INTO video_categories (name, remark) VALUES 
+('AAA', 'aaa');
+
+/** 视频 **/
+DROP TABLE IF EXISTS videos;
+CREATE TABLE IF NOT EXISTS videos (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL DEFAULT '' COMMENT '标题',
+    remark VARCHAR(100) NOT NULL DEFAULT '' COMMENT '备注',
+    cover_image VARCHAR(200) NOT NULL DEFAULT '' COMMENT '封面',
+    duration FLOAT NOT NULL DEFAULT 0 COMMENT '时长',
+    content TExT COMMENT '内容',
+    PRIMARY KEY(id)
+);
+
+/** 评论 **/
+DROP TABLE IF EXISTS video_replies;
+CREATE TABLE IF NOT EXISTS video_replies (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    video_id INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '视频编号',
+    user_id INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户编号',
+    user_name VARCHAR(200) NOT NULL DEFAULT '' COMMENT '用户名称',
+    content TEXT COMMENT '内容',
+    PRIMARY KEY(id)
+);

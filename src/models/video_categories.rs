c4a6a055b96e5607c451e02b5bffc90a83cf9cce
hanting <hanting.hou@gmail.com>
2020-01-14 11:18:50 +0800

    加入分类校验

--- src/models/video_categories.rs
+++ src/models/video_categories.rs
@@ -1,6 +1,8 @@
 use fluffy::{DbRow, model::Model,};
 use super::ModelBackend;
 use serde_derive::{Serialize};
+use std::collections::HashMap;
+use crate::validations::Validator;
 
 #[derive(Default, Debug, Serialize)]
 pub struct VideoCategories { 
@@ -23,4 +25,10 @@ impl ModelBackend for VideoCategories {
         remark => String,
         seq => isize,
     ]);
+
+    fn validate(data: &HashMap<String, String>) -> Result<(), String> { 
+        Validator::load(&data)
+            .length("name", "分类名称必须在2-20之间", 2, 20, true)
+            .validate()
+    }
 }

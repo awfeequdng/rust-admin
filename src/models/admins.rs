261712673c0b1a5ad2550084482590aabaa2d847
hanting <hanting.hou@gmail.com>
2020-01-18 12:14:05 +0800

    补充校验代码

--- src/models/admins.rs
+++ src/models/admins.rs
@@ -1,4 +1,4 @@
-use fluffy::{DbRow, model::Model,};
+use fluffy::{DbRow, model::Model, random, utils};
 use super::ModelBackend;
 use serde_derive::{Serialize};
 use crate::validations::Validator;
@@ -38,12 +38,33 @@ impl ModelBackend for Admins {
         seq => isize,
     ]);
 
+    fn save_before(data: &mut HashMap<String, String>) { 
+        if let Some(v) = data.get("password") {  //如果提交的有密码
+            let secret = random::rand_str(32);
+            let password = utils::get_password(&secret, v);
+            data.insert("password".to_owned(), password);
+            data.insert("secret".to_owned(), secret);
+        }
+    }
+
     fn validate(data: &HashMap<String, String>) -> Result<(), String> { 
-        Validator::load(&data)
-            .string_length("name", "分类名称必须在2-20之间", 2, 20, true)
-            .is_yes_no("state", "状态值不正确")
-            .is_numeric("seq", "排序必须是有效的数字")
-            .validate()
+        let mut vali = Validator::load(&data);
+        let mut need_password = false;
+        if let Some(v) = data.get("id") { 
+            if v == "0" { 
+                need_password = true
+            } else if let Some(_) = data.get("password") { 
+                need_password = true
+            }
+        } 
+        if need_password {
+            vali.is_password("password", "必须输入密码");
+            vali.equal("password", "re_password", "两次输入的密码必须一致");
+        }
+        vali.is_username("name", "请输入正确格式的用户名称", true)
+        .is_yes_no("state", "状态值不正确")
+        .is_numeric("seq", "排序必须是有效的数字")
+        .validate()
     }
 
 }

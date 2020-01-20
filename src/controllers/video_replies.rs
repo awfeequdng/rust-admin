da1ca8bda4efc0b5cb3bf4adc9012d78bb60e45d
hanting <hanting.hou@gmail.com>
2020-01-20 14:25:07 +0800

    修复搜索字段显示

--- src/controllers/video_replies.rs
+++ src/controllers/video_replies.rs
@@ -6,4 +6,13 @@ pub struct VideoReplies { }
 impl Controller for VideoReplies { 
 
     type M = ThisModel;
+
+    fn get_query_cond() -> Vec<(&'static str, &'static str)> { 
+        vec![("video_id", "="), 
+            ("reply_id", "="), 
+            ("user_id", "="), 
+            ("user_name", "%"), 
+            ("created", "[date]"),
+            ("content", "%")] 
+    }
 }

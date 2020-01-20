45769e7b7a95985d829d78fcf6e569f53d92955c
hanting <hanting.hou@gmail.com>
2020-01-20 14:11:20 +0800

    添加查询条件

--- src/controllers/videos.rs
+++ src/controllers/videos.rs
@@ -6,4 +6,8 @@ pub struct Videos { }
 impl Controller for Videos { 
 
     type M = ThisModel;
+
+    fn get_query_cond() -> Vec<(&'static str, &'static str)> { 
+        vec![("title", "%"), ("remark", "%"), ("created", "[date]"), ("updated", "[date]")]
+    }
 }

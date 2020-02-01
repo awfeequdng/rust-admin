99d7c8b49f748ea50e0e1df09ef49ca4e8566de1
hanting <hanting.hou@gmail.com>
2020-02-01 16:45:05 +0800

    修改获取数据库名称方式

--- src/caches/mod.rs
+++ src/caches/mod.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::sync::Mutex;
 use fluffy::{model::Db, db};
-use crate::config::{MYSQL_DB_NAME};
+use crate::config;
 
 /// 状态说明
 pub const STATES: [&'static str; 2] = ["禁用", "正常"];
@@ -10,7 +10,8 @@ lazy_static! {
     /// 数据库-表-字段映射关系
     pub static ref TABLE_FIELDS: Mutex<HashMap<String, Vec<String>>> = {
         let mut conn = db::get_conn();
-        let table_fields = Db::get_table_fields(&mut conn, MYSQL_DB_NAME);
+        let setting = &*config::SETTING;
+        let table_fields = Db::get_table_fields(&mut conn, &setting.database.name);
         Mutex::new(table_fields)
     };
 }

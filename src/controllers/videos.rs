0bc9bb86faa3ccfa84f63184386211c4c5be4250
hanting <hanting.hou@gmail.com>
2020-02-01 19:29:40 +0800

    增加oss参数

--- src/controllers/videos.rs
+++ src/controllers/videos.rs
@@ -1,11 +1,18 @@
 use crate::models::Videos as ThisModel;
 use super::Controller;
+use crate::config;
 
 pub struct Videos { }
 
 impl Controller for Videos { 
 
     type M = ThisModel;
+
+    fn edit_after(data: &mut tera::Context) {
+        let info = config::get_oss_info();
+        data.insert("bucket", &info.bucket);
+        data.insert("end_point", &info.end_point);
+    }
 
     fn get_query_cond() -> Vec<(&'static str, &'static str)> { 
         vec![("title", "%"), ("remark", "%"), ("created", "[date]"), ("updated", "[date]"), ("duration", "[]")]
